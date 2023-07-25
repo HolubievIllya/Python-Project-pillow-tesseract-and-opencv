1c4c9c59903e2c0e9c49df89e6b87455ca9dd903
Illya Holubiev <detrix548@gmail.com>
2023-07-25 21:42:34 +0300

    added files from week 2

---
+++ Week-2-Tesseract-and-Photographs.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'  # your path may be different
+
+
+image=Image.open('readonly/storefront.jpg')
+bounding_box=(315, 170, 700, 270)
+title_image=image.crop(bounding_box)
+title_image.show()
+print(pytesseract.image_to_string(title_image))
---
+++ Week-2-The-Py-Tesseract-Library.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'  # your path may be different
+
+image = Image.open("readonly/text.png")
+
+text = pytesseract.image_to_string(image)
+print(text)
